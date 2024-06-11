667db57a34c7640da52cc8f14b428cad79f64f9f
Andi Duro <nexuister@gmail.com>
2024-06-10 23:05:17 -0400

    Refactored code for better readability and maintainability.

--- Sources/faceprints/commands/add.swift
+++ Sources/faceprints/commands/add.swift
@@ -1,7 +1,6 @@
 import ArgumentParser
 import Foundation
 import Vision
-import Helpers
 
 struct Add: ParsableCommand {
   static var configuration = CommandConfiguration(
@@ -14,16 +13,21 @@ struct Add: ParsableCommand {
     do {
       let labelDir = try createLabelDir(label: args.label)
 
-      let faceImage = try? cropImage(inputImagePath: args.input, boundingBox: VNFaceObservation().boundingBox)
+      let faceImage = try? cropImage(
+        inputImagePath: args.input, boundingBox: VNFaceObservation().boundingBox)
       if faceImage == nil { throw SeeVError.noSubjectFound }
 
       let request = VNGenerateImageFeaturePrintRequest()
-      let featurePrint: [VNFeaturePrintObservation]? = try? performRequest(request: request, input: faceImage!)
+      let featurePrint: [VNFeaturePrintObservation]? = try? performRequest(
+        request: request, input: faceImage!)
       if featurePrint == nil { throw SeeVError.noSubjectFound }
 
-      let embedding = featurePrint!.first!.data.withUnsafeBytes { Array($0.bindMemory(to: Float.self)) }
+      let embedding = featurePrint!.first!.data.withUnsafeBytes {
+        Array($0.bindMemory(to: Float.self))
+      }
       let embeddingData = try JSONSerialization.data(withJSONObject: embedding, options: [])
-      let embeddingFile = labelDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("faceprint")
+      let embeddingFile = labelDir.appendingPathComponent(UUID().uuidString).appendingPathExtension(
+        "faceprint")
       try embeddingData.write(to: embeddingFile)
 
       print("Added faceprint to \(labelDir.path)")
